aed5275e44928e5cf5146142144864f5cc0c43df
isikawa-ak <isikawa.1107.a@gmail.com>
2022-11-10 16:59:39 +0900

    五回勝負に変更しました

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -31,6 +32,12 @@ class _JankenPageState extends State<JankenPage> {
   String myHand = '✊';
   String computerHand = '✊';
   String result = '引き分け';
+  int waveCount = 1;
+  int win = 0;
+  int lose = 0;
+  int draw = 0;
+  bool gameEnd = false;
+  String gameResult = 'No Contest';
   void selectHand(String selectedHand) {
     myHand = selectedHand;
     print(myHand);
@@ -45,19 +52,43 @@ class _JankenPageState extends State<JankenPage> {
   void resultToPrint() {
     if (myHand == computerHand) {
       result = '引き分け';
+      draw++;
       setState(() {});
     } else if (myHand == '✊' && computerHand == '✌' ||
         myHand == '✌' && computerHand == '✋' ||
         myHand == '✋' && computerHand == '✊') {
       result = '勝ち';
+      win++;
       setState(() {});
     } else if (myHand == '✊' && computerHand == '✋' ||
         myHand == '✌' && computerHand == '✊' ||
         myHand == '✋' && computerHand == '✌') {
       result = '負け';
+      lose++;
       setState(() {});
     } else {
       result = '引き分け';
+      setState(() {});
+    }
+  }
+
+  void waveCountToAdd() {
+    waveCount++;
+    if (waveCount >= 6) {
+      gameEnd = !gameEnd;
+      waveResult();
+    }
+  }
+
+  void waveResult() {
+    if (win > lose) {
+      gameResult = 'You Win!';
+      setState(() {});
+    } else if (lose > win) {
+      gameResult = 'You Lose...';
+      setState(() {});
+    } else {
+      gameResult = 'No Contest';
       setState(() {});
     }
   }
@@ -86,59 +117,139 @@ class _JankenPageState extends State<JankenPage> {
         title: Text('ジャンケン'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Text(
-              result,
-              style: TextStyle(
-                fontSize: 32,
+            Visibility(
+              visible: gameEnd,
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      win.toString() +
+                          '勝' +
+                          lose.toString() +
+                          '敗' +
+                          draw.toString() +
+                          '引き分け',
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    Text(
+                      gameResult,
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                    SizedBox(height: 48),
+                    ElevatedButton(
+                        onPressed: () {
+                          waveCount = 1;
+                          win = 0;
+                          lose = 0;
+                          draw = 0;
+                          gameEnd = !gameEnd;
+                          setState(() {});
+                        },
+                        child: Text('もう一度やる')),
+                  ],
+                ),
               ),
             ),
-            // 余白を追加
-            SizedBox(height: 48),
-            Text(
-              computerHand,
-              style: TextStyle(
-                fontSize: 32,
+            Visibility(
+              visible: !gameEnd,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '現在' + waveCount.toString() + '試合目',
+                    style: TextStyle(
+                      fontSize: 32,
+                    ),
+                  ),
+                  // 余白を追加
+                  SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        win.toString() + '勝',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        lose.toString() + '敗',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        draw.toString() + '引き分け',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    result,
+                    style: TextStyle(
+                      fontSize: 32,
+                    ),
+                  ),
+                  // 余白を追加
+                  SizedBox(height: 32),
+                  Text(
+                    computerHand,
+                    style: TextStyle(
+                      fontSize: 32,
+                    ),
+                  ),
+                  // 余白を追加
+                  SizedBox(height: 32),
+                  Text(
+                    myHand,
+                    style: TextStyle(
+                      fontSize: 32,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            selectHand('✊');
+                            generateComputerHand();
+                            resultToPrint();
+                            waveCountToAdd();
+                          },
+                          child: Text('✊')),
+                      ElevatedButton(
+                          onPressed: () {
+                            selectHand('✌');
+                            generateComputerHand();
+                            resultToPrint();
+                            waveCountToAdd();
+                          },
+                          child: Text('✌')),
+                      ElevatedButton(
+                          onPressed: () {
+                            selectHand('✋');
+                            generateComputerHand();
+                            resultToPrint();
+                            waveCountToAdd();
+                          },
+                          child: Text('✋')),
+                    ],
+                  ),
+                ],
               ),
-            ),
-            // 余白を追加
-            SizedBox(height: 48),
-            Text(
-              myHand,
-              style: TextStyle(
-                fontSize: 32,
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      selectHand('✊');
-                      generateComputerHand();
-                      resultToPrint();
-                    },
-                    child: Text('✊')),
-                ElevatedButton(
-                    onPressed: () {
-                      selectHand('✌');
-                      generateComputerHand();
-                      resultToPrint();
-                    },
-                    child: Text('✌')),
-                ElevatedButton(
-                    onPressed: () {
-                      selectHand('✋');
-                      generateComputerHand();
-                      resultToPrint();
-                    },
-                    child: Text('✋')),
-              ],
             ),
           ],
         ),
